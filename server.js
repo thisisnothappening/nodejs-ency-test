0d2ae4ae40a4b2998b90e2a82d928166ca19bbfd
Negoiu <negoiupaulica21@gmail.com>
2023-06-24 01:57:30 +0300

    commit 2

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(errorHandler);
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get('*', (req, res) => {
+app.get('*', function (req, res) {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
